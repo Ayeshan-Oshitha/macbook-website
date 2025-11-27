15ac8eb34ae9b7db4302086186cc438b83e046fb
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-11-27 13:18:45 +0530

    navbar

--- macbook-website/src/App.tsx
+++ macbook-website/src/App.tsx
@@ -1,7 +1,11 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <p>Apple</p>
+      <main>
+        <NavBar />
+      </main>
     </>
   );
 }
---
+++ macbook-website/src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import { navLinks } from "../constants";
+
+const NavBar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <img src="/logo.svg" alt="Apple logo" />
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link.label}>
+                <a href="#">{link.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex-center gap-3">
+            <button>
+              <img src="/search.svg" alt="Search icon" />
+            </button>
+            <button>
+              <img src="/cart.svg" alt="Cart icon" />
+            </button>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
